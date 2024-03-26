7dba155be708fdbff32d1e270ba662f906bd783c
Laurent Ikuzwe <likuzwe19@gmail.com>
2024-03-26 10:12:44 -0500

    JS

---
+++ backyard/Index/js/scipt.js
@@ -0,0 +1,31 @@
+window.addEventListener('resize', function() {
+    adjustStyles();
+});
+
+// Initial call to adjust styles when the page loads
+adjustStyles();
+
+function adjustStyles() {
+    const screenWidth = window.innerWidth;
+    const navbar = document.getElementById('navbar');
+
+    if (screenWidth < 600) {
+        navbar.style.flexDirection = 'column';
+
+        const navbarLinks = document.querySelectorAll('.navbar a');
+        navbarLinks.forEach(link => {
+            link.style.display = 'block';
+            link.style.margin = '10px 0';
+            link.style.textAlign = 'center';
+        });
+    } else {
+        navbar.style.flexDirection = 'row';
+
+        const navbarLinks = document.querySelectorAll('.navbar a');
+        navbarLinks.forEach(link => {
+            link.style.display = 'inline-block';
+            link.style.margin = '0 20px';
+            link.style.textAlign = 'center';
+        });
+    }
+}
